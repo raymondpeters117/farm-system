2c1616a1645d253173fa1def1db89fe1c209d585
raymondpeters117 <raymondpeters117@gmail.com>
2026-06-23 23:00:18 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -145,5 +145,3 @@ function deleteAccount(index) {
 document.addEventListener("DOMContentLoaded", function () {
     displayAccounts();
 });
-localStorage.removeItem("users");
-localStorage.removeItem("loggedInUser");
